44311e16bbe84f3e1f3f5832d1f04dd83f065691
ZeusLMT <lemanhtuan275@gmail.com>
2019-08-15 20:14:18 +0300

    Install Bootstrap for React

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import {Route, Switch} from "react-router-dom";
+import Home from "./Home";
+import Favorites from "./Favorites";
+import Profile from "./Profile";
 
 function App() {
   return (
       <main className={undefined}>
         <Switch>
-          <Route path="/" exact strict component={undefined} />
-          <Route path="/favorite" component={undefined} />
-          <Route path="/profile" component={undefined} />
+          <Route path="/" exact strict component={Home} />
+          <Route path="/favorites" component={Favorites} />
+          <Route path="/profile" component={Profile} />
         </Switch>
       </main>
   );
---
+++ src/containers/Favorites.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Favorites = () => {
+  return (
+      <p>Favorites page</p>
+  );
+};
+
+export default Favorites;
