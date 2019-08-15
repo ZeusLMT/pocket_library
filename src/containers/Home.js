a2fb6ee6c882c0340beb3eab80ef92c158bd6b13
ZeusLMT <lemanhtuan275@gmail.com>
2019-08-16 00:47:51 +0300

    Create Header component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import TabBar from "../components/TabBar";
+import Header from "../components/Header";
+import styles from "./Home.module.css";
+import stylesLayout from "../styles/layout.module.css";
 
 const Home = () => {
   return (
-      <div className='App'>
-        <TabBar/>
-      </div>
+      <>
+        <Header title={`POCKET LIBRARY`} subtitle={`abc`} />
+        {/*<TabBar/>*/}
+      </>
   );
 };
 
